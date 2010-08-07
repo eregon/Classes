57c836bd6b4f782579592cd80d6ff3e415928775
eregon <eregontp@gmail.com>
2010-08-07 12:02:50 +0200

    Add some #to_proc craziness

---
+++ to_proc.rb
@@ -0,0 +1,61 @@
+# A little #to_proc craziness
+# Ideas came from many places
+# Mainly http://rbjl.net/29-become-a-proc-star
+
+class Array
+  def to_proc
+    if Array === first
+      if size == 1
+        procs = first.map(&:to_proc)
+        Proc.new { |obj|
+          procs.map { |sub| sub[obj] }
+        }
+      else
+        procs = map(&:to_proc)
+        Proc.new { |obj|
+          procs.inject(obj) { |result, sub|
+            sub[result]
+          }
+        }
+      end
+    else
+      Proc.new { |obj| obj.send(*self) }
+    end
+  end
+end
+
+if __FILE__ == $0
+  require "rspec"
+
+  describe "Array#to_proc" do
+    it "should behaves like a full block" do
+      ary = [*1..5]
+
+      ary.map(&[:*, 2]).should ==
+      ary.map { |i| i*2 }
+
+      ary.map(&[:to_s]).should ==
+      ary.map { |i| i.to_s }
+
+      ary = %w[f,oo ba,r]
+
+      ary.map(&[:split, ',']).should ==
+      ary.map { |i| i.split ',' }
+    end
+
+    it "should accepts nested Arrays for chaining" do
+      ary = [*1..5]
+      ary.map(&[[:*,2],[:+,4]]).should ==
+      ary.map { |i| i*2 + 4 }
+    end
+
+    it "should return an Array of n results when calling with a single nested Array of n elements" do
+      ary = [*1..5]
+      ary.map(&[[:to_s, :to_f]]).should ==
+      ary.map { |i| [i.to_s, i.to_f] }
+
+      ary.map(&[[[:to_s, 2], :to_f]]).should ==
+      ary.map { |i| [i.to_s(2), i.to_f] }
+    end
+  end
+end
